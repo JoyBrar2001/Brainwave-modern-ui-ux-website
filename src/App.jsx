30190d77dcb26a68e7a4c08280780eeb980fff53
Joy Brar <joybrar2001@gmail.com>
2024-03-08 18:26:24 +0530

    Adding the services

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Button from "./components/Button";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
       </div>
 
       <ButtonGradient />
--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -4,11 +4,17 @@ const Generating = ({ className }) => {
   return (
     <div className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${className || ""} text-base`}>
       <img
-        className="w-5 h-5 mr-4"
         src={loading}
         alt="Loading"
+        className="w-5 h-5 mr-4"
+        style={{
+          animation: 'spin 1s linear infinite',
+          '@keyframes spin': {
+            '100%': { transform: 'rotate(-360deg)'}
+          }
+        }}
       />
-      AI is generating |
+      AI is generating<span className="animate-pulse ml-0.5">|</span>
     </div>
   )
 }
