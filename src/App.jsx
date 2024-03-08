2db7d854b398d571e0285d040bf6cdac86e566d9
Joy Brar <joybrar2001@gmail.com>
2024-03-09 02:14:59 +0530

    Finished the website

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 const App = () => {
@@ -15,6 +18,9 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
 
       <ButtonGradient />
